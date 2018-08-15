e5ba9af081acdbb2c5e5b60c6b45fb3a5d2da2cd
Mark Grennan <mgrennan@hotmail.com>
2018-08-15 15:32:54 +0100

    "Initial notepad with box and post working"

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, session, request
+
+app = Flask(__name__)
+
+notes = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
+
+    return render_template("index.html", notes=notes)
